229b522ef7f057b6116a5c44efbe92b7b7490da6
Haley Lee <hal64@cornell.edu>
2021-01-24 21:44:27 -0500

    Finished Roommates case study

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,11 +1,18 @@
+var limit = 195;
+
+if (document.body.classList.contains('casestudy')) {
+  limit = 700;
+}
+
 $(window).scroll(function() {
   var height = $(window).scrollTop();
+  // var limit = 195;
 
   if ($(window).width() < 600) {
-    
+
   }
   else {
-    if(height  > 195) {
+    if(height  > limit) {
       $("#side").fadeIn(250);
     }
     else {
